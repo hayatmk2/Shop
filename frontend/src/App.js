fd2dcf0f2869d34c7eaa09c0f093bbe523f5a783
Hayat Kemal <hayatmk4@gmail.com>
2022-11-15 17:15:32 -0800

    Rating component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import {Container} from 'react-bootstrap'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () =>{
   return(
     <>
+    <Router>
     <Header/>
     <main>
         <Container>
-          <HomeScreen/>
+          <Routes>
+            <Route path="/" element={<HomeScreen/>}  exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
     </main>
     <Footer/>
+    </Router>
     </>  
   );
 }
